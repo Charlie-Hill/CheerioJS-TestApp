999bfdaf7b44a92833fdb98d0b5779475e0f0b81
Charlie <30829462+Charlie-Hill@users.noreply.github.com>
2022-01-07 20:53:57 +0000

    Add twitter scraper test

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const http = require('http');
 const cheerio = require('cheerio');
 const request = require('request');
+const fs = require('fs')
 
 // Create an instance of the http server to handle HTTP requests
 let app = http.createServer((req, res) => {
@@ -10,11 +11,31 @@ let app = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type": "text/plaintext"});
 
     let retHTML = '';
-    request(`https://www.google.com`, (error, response, html) => {
+    request(`https://twitter.com/hashtag/bids?src=hash&f=live`, (error, response, html) => {
         if (!error && response.statusCode === 200) {
             const $ = cheerio.load(html);
+
+            // fs.writeFile('./output.txt', $.html(), err => {
+            //     if (err) {
+            //         console.error(err)
+            //         return
+            //     }
+            // })
+              
+
+
+            $('li.stream-item').each(function(index){
+                var name = $(this).find('.fullname').text();
+                var screenname = $(this).find('span.username').text();
+                var tweet = $(this).find('p.tweet-text').text();
+                
+                console.log('user : ' + name);   //name of the user
+                console.log('screen name : ' + screenname); //screenname of user 
+                console.log('tweet : ' + tweet);   //tweet content
+                
+                res.write(name + "\n" + tweet + "\n\n");
+            });
             
-            res.write(html)
             
         } else {
             throw new error(error)
